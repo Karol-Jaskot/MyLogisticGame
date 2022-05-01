124382d5a266a3285fac44ea40e658a29eb0a7d7
Karol-Jaskot <karol.jaskot97@gmail.com>
2022-05-01 10:04:57 +0200

    Fix database path configuration

--- configs/DatabaseeConfigurtation.go
+++ configs/DatabaseeConfigurtation.go
@@ -1,11 +1,22 @@
 package configs
 
 import (
+	"errors"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"os"
 )
 
 func GetConnection() *gorm.DB {
+	filename := "./database/MainDB.db"
+
+	if _, err := os.Stat(filename); errors.Is(err, os.ErrNotExist) {
+		os.MkdirAll("./database", 0755)
+		os.Create(filename)
+	} else {
+		// file exists
+	}
+
 	db, err := gorm.Open(sqlite.Open("database/MainDB.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
